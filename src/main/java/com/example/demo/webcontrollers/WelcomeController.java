627738b79a839555d847f1655360a089b8493888
Winter, Achim <acw@adorsys.de>
2019-05-21 14:38:13 +0200

    added thymeleaf

--- src/main/java/com/example/demo/webcontrollers/WelcomeController.java
+++ src/main/java/com/example/demo/webcontrollers/WelcomeController.java
@@ -2,21 +2,38 @@ package com.example.demo.webcontrollers;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Map;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class WelcomeController {
 
     // inject via application.properties
-    @Value("${hello.message:test}")
-    private String message = "Hello World";
+    @Value("${welcome.message}")
+    private String message;
 
-    @RequestMapping("/")
-    public String welcome(Map<String, Object> model) {
-        model.put("message", this.message);
-        return "hello";
+    private List<String> tasks = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("message", message);
+        model.addAttribute("tasks", tasks);
+
+        return "welcome"; //view
+    }
+
+    @GetMapping("/hello")
+    public String mainWithParam(
+            @RequestParam(name = "name", required = false, defaultValue = "")
+                    String name, Model model) {
+
+        model.addAttribute("message", name);
+
+        return "welcome"; //view
     }
 
 }
