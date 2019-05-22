26c2a7960284e57331e9ded19644bc1b0ebd3e62
Winter, Achim <acw@adorsys.de>
2019-05-22 08:55:43 +0200

    fixed things

--- src/main/java/com/example/demo/models/OrderList.java
+++ src/main/java/com/example/demo/models/OrderList.java
@@ -13,7 +13,7 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-class OrderList {
+public class OrderList {
 
     @Id
     @GeneratedValue
--- src/main/java/com/example/demo/repositories/OrderListRepository.java
+++ src/main/java/com/example/demo/repositories/OrderListRepository.java
@@ -1,11 +1,9 @@
 package com.example.demo.repositories;
 
-import com.example.demo.models.Order;
+import com.example.demo.models.OrderList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface OrderListRepository extends JpaRepository<List<Order>, Long> {
+public interface OrderListRepository extends JpaRepository<OrderList, Long> {
 }
